fe469e982e289a53e24a922b989aee7d876696e7
mouhamedlamotte <mouhamedlamotte.dev@gmail.com>
2024-12-16 20:33:13 +0000

    doc : explain kdebug

--- src/utils/kdebug.ts
+++ src/utils/kdebug.ts
@@ -1,3 +1,10 @@
+
+
+/**
+ * Logs messages to the console if the environment is set to development.
+ * @param {...any[]} args - The messages to log.
+ */
+
 export const kdebug = (...args: any[]) => {
     if (process.env.NODE_ENV === 'development') {
         console.debug(...args);
